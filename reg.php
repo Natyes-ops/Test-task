8cc9f10b382753c2d6cad947e76f0d8a1723bc33
Natyes-ops <gurit.nat@bk.ru>
2022-11-10 12:49:36 +0300

    fix: change regex pattern

--- reg.php
+++ reg.php
@@ -24,15 +24,15 @@ if (isset($_SESSION['user'])) {
         <form class="form" id="registration" action="./app/registration.php" method="POST">
             <label for="name" class="form-item">
                 Имя:
-                <input type="text" name="name" placeholder="Введите имя" class="form-item__input" title="Введите имя на латинице, минимум 2 символа" pattern="[a-zA-Z]{2,}" required>
+                <input type="text" name="name" placeholder="Введите имя" class="form-item__input" title="Введите имя на латинице, минимум 2 символа" pattern="\w[a-zA-Z]{1,}" required>
             </label>
             <label for="login" class="form-item">
                 Логин:
-                <input type="text" name="login" placeholder="Введите логин" class="form-item__input" minlength="6" required>
+                <input type="text" name="login" placeholder="Введите логин" class="form-item__input" minlength="6" required pattern="^\S+[a-zA-Z]{2,}\S+$">
             </label>
             <label for="password" class="form-item">
                 Пароль:
-                <input type="password" name="password" placeholder="Введите пароль" class="form-item__input" title="минимум 6 символов, обязательно должны быть цифры и буквы" pattern="(?=.*\d)(?=.*[a-z]).{6,}" required>
+                <input type="password" name="password" placeholder="Введите пароль" class="form-item__input" title="минимум 6 символов, обязательно должны быть цифры, буквы и спец. символы !@#$&*" pattern="^\S(?=.*\d)(?=.*[!@#$&*])(?=.*[a-z]).{6,}\S+$" required>
             </label>
             <label for="password-check" class="form-item">
                 Повторите пароль:
@@ -40,7 +40,7 @@ if (isset($_SESSION['user'])) {
             </label>
             <label for="email" class="form-item">
                 Почта:
-                <input type="email" name="email"  placeholder="Введите почту" class="form-item__input" title="Введите в формате test@test.ru" pattern="[a-z0-9]+@[a-z0-9]+\.[a-z]{2,}$" required>
+                <input type="email" name="email"  placeholder="Введите почту" class="form-item__input" title="Введите в формате test@test.ru" pattern="^\S[a-z0-9]+@[a-z0-9]+\.[a-z]{1,}\S+$" required>
             </label>
             <button class="btn" type="submit">Зарегистрироваться</button>
             <a href="/">Авторизация</a>
